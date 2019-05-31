1da931fb904c56718204db486d91a03a27b8860a
qiuquan <qiuquan@qdcares.cn>
2019-05-31 11:24:38 +0800

    'immutable组件加入'

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -46,60 +46,17 @@ const Header = (props) => {
     )
 }
 
-// class Header extends Component {
-//
-//   render() {
-//     return (
-//       <HeaderWapper>
-//         <Logo/>
-//         <Nav>
-//           <NavItem className="left active">首页</NavItem>
-//           <NavItem className="left">下载App</NavItem>
-//           <NavItem className="right">登陆</NavItem>
-//           <NavItem className="right"><i className="iconfont">&#xe636;</i></NavItem>
-//           <SearchWapper>
-//             <CSSTransition
-//               timeout={200}
-//               in={this.props.focused}
-//               classNames="slide"
-//             >
-//               <NavSearch
-//                 className={this.props.focused ? 'focused' : ''}
-//                 onFocus={this.props.handleInputFocus}
-//                 onBlur={this.props.handleInputBlur}
-//               >
-//               </NavSearch>
-//             </CSSTransition>
-//             <i  className={this.props.focused ? 'focused iconfont' : 'iconfont'}>&#xe6cf;</i>
-//           </SearchWapper>
-//         </Nav>
-//         <Addition>
-//           <Button className='writting'><i className="iconfont">&#xe670;</i>写文章</Button>
-//           <Button className='reg'>注册</Button>
-//         </Addition>
-//       </HeaderWapper>
-//     )
-//   }
-//
-// }
-
 const mapStateToProps = (state) => {
   return {
-    focused: state.header.focused,
+    focused: state.header.get('focused'), // immutable  get方法
   }
 }
 const mapDispathToProps = (dispatch) => {
   return {
     handleInputFocus() {
-      const action = {
-        type: 'search_focus',
-      };
       dispatch(actionCreator.searchFoucs())
     },
     handleInputBlur() {
-      const action = {
-        type: 'search_blur',
-      };
       dispatch(actionCreator.searchBlur())
     }
   }
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,19 +1,16 @@
-import * as contants from './constants'
+import * as contants from './constants';
+import { fromJS } from 'immutable';
 
-const defaultState = {
+const defaultState = fromJS({ // fromJS把js对象转成immutable对象
   focused: false, // 输入框动画标志
-}; // 默认值
+}); // 默认值
 
 export default (state = defaultState, action) => { // 返回纯函数
   if (action.type === contants.SEARCH_FOCUS) {
-    return {
-      focused: true,
-    }
+    return  state.set('focused', true); // immutable对象的set方法会结合之前的immutable对象的值和设置的值，返回一个全新对象
   }
   if (action.type === contants.SEARCH_BLUR) {
-    return {
-      focused: false,
-    }
+    return  state.set('focused', false); // immutable对象的set方法会结合之前的immutable对象的值和设置的值，返回一个全新对象
   }
   return state;
 }
