108f564a723b2912e2e0e27d879f7479b0c1477a
qiuquan <qiuquan@qdcares.cn>
2019-05-31 11:01:16 +0800

    '123'

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,17 +1,8 @@
-const defaultState = {
-  focused: false, // 输入框动画标志
-}; // 默认值
+import { combineReducers  } from 'redux'; // 整合reducer
+import headerReducer from '../common/header/store/reducer';
 
-export default (state = defaultState, action) => { // 返回纯函数
-  if (action.type === 'search_focus') {
-    return {
-      focused: true,
-    }
-  }
-  if (action.type === 'search_blur') {
-    return {
-      focused: false,
-    }
-  }
-  return state;
-}
+const reducer = combineReducers({
+  header: headerReducer
+})
+
+export default reducer;
